489639257ab0dd0b70bcec7fde98a7ab4393125e
Romain Bertrand <romain@doxin.net>
2024-11-19 12:02:10 +0100

    Configure font in wezterm

--- .wezterm.lua
+++ .wezterm.lua
@@ -2,5 +2,7 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 config.color_scheme = 'Catppuccin Mocha'
+config.font = wezterm.font 'FiraCode Nerd Font Mono'
+config.max_fps = 165
 
 return config
