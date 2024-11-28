ba14124abbc14c48fda0f79401f71aed005ad6c2
Romain Bertrand <romain@doxin.net>
2024-11-28 18:04:20 +0100

    November update

--- .wezterm.lua
+++ .wezterm.lua
@@ -2,7 +2,9 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 config.color_scheme = 'Catppuccin Mocha'
-config.font = wezterm.font 'FiraCode Nerd Font Mono'
+config.font = wezterm.font('FiraCode Nerd Font', { weight = 'Medium' })
+config.underline_position = -2
 config.max_fps = 165
+config.enable_tab_bar = false
 
 return config
