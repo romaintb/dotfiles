844ce615204ca23c46dd216a1211f454b2b9b1da
Romain Bertrand <romain@doxin.net>
2025-05-21 15:33:51 +0200

    Update wezterm's font config

--- .wezterm.lua
+++ .wezterm.lua
@@ -3,8 +3,10 @@ local config = wezterm.config_builder()
 
 config.color_scheme = 'Catppuccin Mocha'
 config.font = wezterm.font('FiraCode Nerd Font', { weight = 'Medium' })
+config.font_size = 9.0
 config.underline_position = -2
 config.max_fps = 165
 config.enable_tab_bar = false
+config.warn_about_missing_glyphs = false
 
 return config
