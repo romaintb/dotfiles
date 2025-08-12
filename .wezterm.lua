101c788b0810bbb90415093a366f0d0a538dfa35
Romain Bertrand <romain@doxin.net>
2025-08-12 20:05:32 +0200

    Meaningful git commit message.

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,12 +1,16 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
-config.color_scheme = 'Catppuccin Mocha'
+-- config.color_scheme = 'Catppuccin Mocha'
+config.color_scheme = 'Github Dark (Gogh)'
 config.font = wezterm.font('FiraCode Nerd Font', { weight = 'Medium' })
 config.font_size = 9.0
 config.underline_position = -2
 config.max_fps = 165
 config.enable_tab_bar = false
 config.warn_about_missing_glyphs = false
+config.colors = {
+  foreground = 'white'
+}
 
 return config
