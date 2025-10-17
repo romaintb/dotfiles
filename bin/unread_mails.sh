57d5565680aee00a69f4451971a2509406ff9442
Romain Bertrand <romain@doxin.net>
2025-10-17 12:28:13 +0200

    Remove unused bin/

--- bin/unread_mails.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-MAILS=`find ~/.maildir -path "*/new/*"| grep -v spam | grep -v ob- | wc -l | tr -d " "`
-MAILS_ML=`find ~/.maildir -path "*/new/*"| grep -v spam | grep ob- | wc -l | tr -d " "`
-echo "ml : $MAILS_ML | mails : $MAILS"
