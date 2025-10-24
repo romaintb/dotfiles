960d1e5de74d76fa3c695e11f3516d81ee5b7b4c
Romain Bertrand <romain@doxin.net>
2025-10-24 11:58:13 +0200

    Configure different colorscheme for some directories

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -1,7 +1,60 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
--- config.color_scheme = 'Catppuccin Mocha'
+-- Define Dracula Soft color scheme
+config.color_schemes = {
+  ['Dracula Soft'] = {
+    foreground = '#e9e9f4',
+    background = '#282a36',
+    cursor_bg = '#f8f8f2',
+    cursor_border = '#f8f8f2',
+    cursor_fg = '#282a36',
+    selection_bg = '#44475a',
+    selection_fg = '#e9e9f4',
+
+    ansi = {
+      '#21222c', -- black
+      '#e8616a', -- red (soft)
+      '#8aff80', -- green (soft)
+      '#ffca80', -- yellow (soft)
+      '#bd93f9', -- blue
+      '#ff79c6', -- magenta
+      '#8be9fd', -- cyan
+      '#e9e9f4', -- white (soft)
+    },
+
+    brights = {
+      '#6272a4', -- bright black
+      '#e8616a', -- bright red (soft)
+      '#8aff80', -- bright green (soft)
+      '#ffca80', -- bright yellow (soft)
+      '#bd93f9', -- bright blue
+      '#ff79c6', -- bright magenta
+      '#8be9fd', -- bright cyan
+      '#f7f7fb', -- bright white
+    },
+  },
+}
+
+-- Dynamic theme switching based on current working directory
+wezterm.on('update-status', function(window, pane)
+  local cwd = pane:get_current_working_dir()
+  if cwd then
+    local cwd_path = cwd.file_path or ''
+    -- Check if we're in the cocoon-immo-bis directory
+    if cwd_path:match('/work/cocoon%-immo%-bis') then
+      window:set_config_overrides({
+        color_scheme = 'Dracula Soft',
+      })
+    else
+      window:set_config_overrides({
+        color_scheme = 'Catppuccin Macchiato',
+      })
+    end
+  end
+end)
+
+-- Default theme
 config.color_scheme = 'Catppuccin Macchiato'
 config.font = wezterm.font('FiraCode Nerd Font', { weight = 'Medium' })
 config.font_size = 9.0
